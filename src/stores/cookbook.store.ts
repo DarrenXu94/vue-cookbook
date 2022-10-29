0e904f168377099ea56af8787ccaa2f59921f46a
Darren Xu <darrenxu1994@gmail.com>
2022-10-30 10:53:46 +1100

    Individual pages

--- src/stores/cookbook.store.ts
+++ src/stores/cookbook.store.ts
@@ -1,11 +1,22 @@
 import { defineStore } from "pinia";
 import { CookbookResponse } from "../models/cookbook.models";
 import { CookbookService } from "../services/cookbook.service";
+import snakeCase from "lodash.snakecase";
 
 const cookbookService = new CookbookService();
 
 export const useCookbookStore = defineStore("cookbook", {
   state: () => ({ recipes: null as unknown as CookbookResponse }),
+  getters: {
+    getRecipeByName: (state) => {
+      return (name: string) =>
+        state.recipes.results.find(
+          (recipe) =>
+            recipe.properties.Name.title.length &&
+            snakeCase(recipe.properties.Name.title[0].text.content) === name
+        );
+    },
+  },
   actions: {
     async initStore() {
       const res = await cookbookService.getCookbook();
--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,17 +1,57 @@
 <template>
-  <div>Recipe</div>
+  <div class="recipe-page">
+    <h1>
+      {{ recipe?.properties.Name.title[0].text.content }}
+    </h1>
+    <div>
+      Meal type
+      <div v-for="mealType of recipe?.properties['Meal type'].multi_select" :key="mealType.id">
+        {{ mealType.name }}
+      </div>
+    </div>
+    <div>
+      Ingredients
+      <div v-for="ingredient of recipe?.properties.Ingredients.multi_select" :key="ingredient.id">
+        {{ ingredient.name }}
+      </div>
+    </div>
+    <div>
+      Steps
+      <div v-for="steps of recipe?.properties.Steps.rich_text" :key="steps.plain_text">
+        <div v-html="steps.text.content">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Result } from '../models/cookbook.models';
+import { useCookbookStore } from '../stores/cookbook.store';
 
 export default defineComponent({
   name: 'Recipie',
   components: {
   },
+  beforeMount() {
+    this.recipe = this.cookbook.getRecipeByName(this.$route.params.id as string)
+  },
+  setup() {
+    const cookbook = useCookbookStore();
+    return {
+      cookbook
+    }
+  },
   data() {
     return {
+      recipe: undefined as Result | undefined
     };
   },
 });
 </script>
+<style lang="scss">
+.recipe-page {
+  white-space: pre-line;
+}
+</style>
