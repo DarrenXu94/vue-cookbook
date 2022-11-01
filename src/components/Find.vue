bcaee042dee0d4ac841aee0174b5003c0571d6d1
Darren Xu <darrenxu1994@gmail.com>
2022-11-01 19:13:25 +1100

    Iconify

--- src/components/Find.vue
+++ src/components/Find.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="find-component">
     <Pill>
+      <div class="icon-span">
+        <Icon icon="bytesize:search" />
+      </div>
+
       <Input />
     </Pill>
   </div>
@@ -10,12 +14,14 @@
 import { defineComponent } from 'vue';
 import Input from './ui/Input.vue'
 import Pill from './ui/Pill.vue'
+import { Icon } from '@iconify/vue';
 
 export default defineComponent({
   name: 'Find',
   components: {
     Input,
-    Pill
+    Pill,
+    Icon
   },
   data() {
     return {
@@ -27,12 +33,23 @@ export default defineComponent({
 .find-component {
   .pill {
     padding: 0px;
+    position: relative;
+
+    .icon-span {
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translate(50%, -50%);
+      display: flex;
+
+    }
 
     .custom-input {
       input {
         border-radius: var(--border-radius);
         border: none;
         margin: 0;
+        padding-left: 40px;
 
         &::placeholder {
           text-align: center;
